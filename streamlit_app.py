8660eeb2143e4529df248969ff4382f52854399b
Mathias Renschler <Mathias.Renschler@Blueforte.com>
2024-07-09 18:23:21 +0200

    displaying JSON with fruityvice json as table

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,4 +49,5 @@ if ingredients_list:
 # for now only for a fixed type of fruit
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+# st.text(fruityvice_response.json())
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
