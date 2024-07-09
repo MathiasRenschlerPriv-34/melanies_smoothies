874ce78d9823c08bc278317fb06d5768539a33eb
Mathias Renschler <Mathias.Renschler@Blueforte.com>
2024-07-09 18:06:40 +0200

    Added option to display fruityvice nutrition information

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,3 +44,9 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()       
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
+
+# New section to display fruityvice nutrition information
+# for now only for a fixed type of fruit
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
